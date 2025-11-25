29b977687c2ec3d4d08c8a37415547e0e0e88d2b
ISoldatov <isoldatov33@yandex.ru>
2025-11-25 20:07:31 +0300

    lesson_3 introduce interface

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -6,8 +6,10 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes.
  */
-public class ArrayStorage {
-    private final Resume[] storage = new Resume[10000];
+public class ArrayStorage implements Storage {
+    private static final int STORAGE_LIMIT = 10000;
+
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
 
     private int size = 0;
 
@@ -21,7 +23,7 @@ public class ArrayStorage {
     public void save(Resume r) {
         if (getIndex(r.getUuid()) >= 0) {
             System.out.println("ОШИБКА! Резюме с uuid=" + r.getUuid() + " уже присутсвует в хранилище.");
-        } else if (size == storage.length) {
+        } else if (size == STORAGE_LIMIT) {
             System.out.println("ОШИБКА! Хранилище переполнено.");
         } else {
             storage[size] = r;
